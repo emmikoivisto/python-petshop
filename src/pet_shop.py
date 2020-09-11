381af6a535590b88ee0481d152ed2751b61bdc6b
emmikoivisto <emetsi@gmail.com>
2020-09-11 17:21:53 +0100

    some additions

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -6,3 +6,14 @@ def get_pet_shop_name(pet_shop_dictionary):
 def get_total_cash(sum_of_cash):
     return sum_of_cash["admin"]["total_cash"]
 
+
+#def add_or_remove_cash(total, num1):
+#    return total["admin"]["total_cash"] + num1
+
+
+def get_pets_sold(sold_pets):
+    return sold_pets["admin"]["pets_sold"]
+
+
+#def get_stock_count(number_of_pets):
+#    return len["cc_pet_shop"]
