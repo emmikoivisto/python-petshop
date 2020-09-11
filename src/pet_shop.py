95b9e54faad0b4887f7a60ca99594a9ba2d7ae51
emmikoivisto <emetsi@gmail.com>
2020-09-11 16:34:16 +0100

    second function added

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,3 +1,8 @@
 # WRITE YOUR FUNCTIONS HERE
 def get_pet_shop_name(pet_shop_dictionary):
     return pet_shop_dictionary["name"]
+
+
+def get_total_cash(sum_of_cash):
+    return sum_of_cash["admin"]["total_cash"]
+
